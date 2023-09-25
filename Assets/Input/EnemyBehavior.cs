73c45668db0faaa7e355ca14d3be8c0543a65c54
Tan <looktan_lnw@hotmail.com>
2023-09-25 18:35:02 +0700

    Enemy
    -follow player
    -Point a to point b

---
+++ Assets/Input/EnemyBehavior.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform[] WayPoints;
+    [SerializeField] private float MoveSpeed = 3f;
+    [SerializeField] private float DetectionRadius = 5f;
+    private int CurrentWaypointIndex = 0;
+    private Transform Player;
+    private bool FollowingPlayer = false;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.position) <= DetectionRadius)
+        {
+            FollowingPlayer = true;
+        }
+        else
+        {
+            FollowingPlayer = false;
+        }
+
+        if (FollowingPlayer)
+        {
+            Vector3 targetDirection = Player.position - transform.position;
+            targetDirection.Normalize();
+            transform.Translate(targetDirection * MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 targetDirection = WayPoints[CurrentWaypointIndex].position - transform.position;
+            targetDirection.Normalize();
+            transform.Translate(targetDirection * MoveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, WayPoints[CurrentWaypointIndex].position) < 0.1f)
+            {
+                CurrentWaypointIndex = (CurrentWaypointIndex + 1) % WayPoints.Length;
+            }
+        }
+    }
+}
