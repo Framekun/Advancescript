5e0be42fecf936c16c8e0c8867cf9052726ab7b6
Tan <looktan_lnw@hotmail.com>
2023-10-23 16:14:34 +0700

    Player Hp/Player lost or die
    -tell player hp in debug.log
    -player lost hp when collide enemy
    -player die when lost all hp(Destroy object)

--- Assets/Scripts/Player/PlayerHP.cs
+++ Assets/Scripts/Player/PlayerHP.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
-    [SerializeField] private float HP;
+    [SerializeField] private float playerHP;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            playerHP--;
+            Debug.Log("Player HP: " + playerHP);
+        }
+        if(playerHP <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over");
+        }
+    }
+
     void Update()
     {
         
