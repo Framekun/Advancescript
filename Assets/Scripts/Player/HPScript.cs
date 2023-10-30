c1a36bb4772829e330452169594d650a61fb9b9d
Framekun <70421400+Framekun@users.noreply.github.com>
2023-10-30 15:02:32 +0700

    Make HP as Scriptable object

---
+++ Assets/Scripts/Player/HPScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject")]
+public class HPScript : ScriptableObject
+{
+    public string name;
+    public float hp;
+}
--- Assets/Scripts/Player/PlayerHP.cs
+++ Assets/Scripts/Player/PlayerHP.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class PlayerHP : MonoBehaviour
 {
-    [SerializeField] private float playerHP;
+    [SerializeField] private float PlayerCurrentHP;
+    [SerializeField] private HPScript Hploader;
     void Start()
     {
-        
+        PlayerCurrentHP = Hploader.hp;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            playerHP--;
-            Debug.Log("Player HP: " + playerHP);
+            PlayerCurrentHP--;
+            Debug.Log("Player HP: " + PlayerCurrentHP);
         }
-        if(playerHP <= 0)
-        {
-            Destroy(gameObject);
-            Debug.Log("Game Over");
-        }
+        
     }
 
     void Update()
     {
-        
+        if (PlayerCurrentHP <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over");
+        }
     }
 }
